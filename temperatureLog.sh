676859dd3496d244f63a03bfa15987b8489e97e6
Raju Vindane <rajuvindane@cryptolab.net>
2015-03-31 17:43:08 +0530

    A handy script to Log the Temperature of BeagleBone

---
+++ temperatureLog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+timestamp=`date +%F_%H-%M-%S`
+echo "Temperature Log - $(date)" >/root/TemperatureLogs/temperature_log_$timestamp.txt
+while :
+do
+#	temp=`sensors`
+	sensors
+#	temp=${temp:5:16}
+	echo $temp >>/root/TemperatureLogs/temperature_log_$timestamp.txt
+	sleep 60
+done
